b5bc40b1ea6d06ac99f99d53485eacb3afdcef26
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-09-19 21:04:45 +0200

    update

--- DiseñodeInterfaces/EvaluacionInicial/index.js
+++ DiseñodeInterfaces/EvaluacionInicial/index.js
@@ -6,5 +6,19 @@ function recibirDatos() {
     let correo = document.getElementById('correo').value;
 
     console.log(nombre, apellidos, fechaDeNac, correo);
+
+    var divElement = document.getElementById('formulario');
+
+    alert("Enviado satisfactoriamente, ahora se verá tu información personal")
+    
+    divElement.innerHTML = `
+    <h1>Comprobación de datos</h1>
+    <p >Nombre: ${nombre} </p>
+    <p> Apellidos: ${apellidos} </p>
+    <p> Fecha de nacimiento: ${fechaDeNac} </p>
+    <p> Correo de contacto: ${correo} </p>
+
+    `
+    
     
 }
