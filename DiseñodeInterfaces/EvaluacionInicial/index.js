ee15a106a3be42cadb8550182f2e57d55065c3eb
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-09-19 20:52:40 +0200

    update

---
+++ DiseñodeInterfaces/EvaluacionInicial/index.js
@@ -0,0 +1,10 @@
+function recibirDatos() {
+    //alert("Hello World");
+    let nombre = document.getElementById('nombre').value;
+    let apellidos = document.getElementById('apellidos').value;
+    let fechaDeNac = document.getElementById('edad').value;
+    let correo = document.getElementById('correo').value;
+
+    console.log(nombre, apellidos, fechaDeNac, correo);
+    
+}
