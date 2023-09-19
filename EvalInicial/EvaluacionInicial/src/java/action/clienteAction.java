3edae57480a57f56deff9c8354f3dd79d08c89ad
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-09-19 12:10:32 +0200

    asdf

---
+++ EvalInicial/EvaluacionInicial/src/java/action/clienteAction.java
@@ -0,0 +1,26 @@
+package java.action;
+
+import javax.servlet.http.HttpServletRequest;
+import java.interfaces.IAction;
+
+public class clienteAction implements IAction {
+    @Override
+    public String execute(HttpServletRequest req, String Action) {
+        return null;
+    }
+
+    @Override
+    public String update(HttpServletRequest req) {
+        return null;
+    }
+
+    @Override
+    public String add(HttpServletRequest req) {
+        return null;
+    }
+
+    @Override
+    public String delete(HttpServletRequest req) {
+        return null;
+    }
+}
--- EvalInicial/EvaluacionInicial/src/java/interfaces/IAction.java
+++ EvalInicial/EvaluacionInicial/src/java/interfaces/IAction.java
@@ -6,4 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 public interface IAction  {
     public String execute (HttpServletRequest req, String Action);
     public String update (HttpServletRequest req);
+    public String add (HttpServletRequest req);
+    public String delete (HttpServletRequest req);
+
 }
