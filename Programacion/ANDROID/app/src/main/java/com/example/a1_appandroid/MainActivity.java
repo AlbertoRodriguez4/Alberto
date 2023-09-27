1c8514a0100ffc5ce26c52cd4f2554f19ac2ea55
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-09-27 12:05:19 +0200

    update

--- Programacion/ANDROID/app/src/main/java/com/example/a1_appandroid/MainActivity.java
+++ Programacion/ANDROID/app/src/main/java/com/example/a1_appandroid/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.a1_appandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtUser;
+    private EditText edtPassword;
+    private Button btnSignin;
 
+    /*CICLO DE VIDA: ONLOAD JAVASCRIPT*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //document.getElementById("userId)
+        EditText edtUser = findViewById(R.id.edtUser);
+        EditText edtPassword = findViewById(R.id.edtPassword);
+        Button btnSignin = findViewById(R.id.btnSignin);
+        btnSignin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        }
     }
 }
