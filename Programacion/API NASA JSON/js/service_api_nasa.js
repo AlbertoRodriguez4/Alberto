e8a55f820e435fe0848b80b5e56fbc66f3b7df25
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-09-28 21:13:27 +0200

    update

--- Programacion/API NASA JSON/js/service_api_nasa.js
+++ Programacion/API NASA JSON/js/service_api_nasa.js
@@ -8,20 +8,14 @@ const apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?
 
 // Realizar una solicitud GET utilizando Fetch API
 fetch(apiUrl)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error('Cristiano Ronaldo');
-    }
-    return response.json();
-  })
-  .then((data) => {
-    // Los datos de las fotos se encuentran en el objeto 'data'
-    console.log(data);
-    // Aquí puedes procesar los datos o mostrar las imágenes en tu aplicación
-    for (var i = 1; i < 100; i++) {
-      console.log("hola")
-    }
-  })
+.then(response => response.json())
+.then(json => {
+  console.log(json)
+ json.array.forEach(element => {
+  console.log(element)
+  
+ });
+})
 
   .catch((error) => {
     console.error('Cristiano Ronaldo:', "siuuu");
