838b53f077228c67cf69d70489b8b35db8812272
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-10-03 12:54:33 +0200

    asdf

--- DiseñodeInterfaces/LoginEnPhp/index.php
+++ DiseñodeInterfaces/LoginEnPhp/index.php
@@ -1,17 +1,20 @@
 <?php session_start();
-if(isset($_SESSION['usuario'])&& $_SESSION['usuario']!='') {
-$mensa = "Estas logueado crack";
+if (isset($_SESSION['usuario']) && $_SESSION['usuario'] != '') {
+    $mensa = "Estas logueado crack";
 } else {
-header('Location: index.php');
+    header('Location: index.php');
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 </head>
+<meta name="viewport" content="width">
 
 <body>
-    Hola <?php echo $_SESSION['usuario']; ?>.    <br>
+    <section id="secEncabezadoPágina" class="container-fluid"></section>
+    Hola <?php echo $_SESSION['usuario']; ?>. <br>
 </body>
 
 </html>
