6eba722507dac2e90325c5dc8651dabc27c6dcc3
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-10-04 10:23:41 +0200

    update

--- Programacion/ASDF/app/src/main/java/com/example/login_03_10_23/MainActivity.java
+++ Programacion/ASDF/app/src/main/java/com/example/login_03_10_23/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.login_03_10_23;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtEmail);
+        btnLogin = findViewById(R.id.btnLogin);
+    }
+
+    private void initComponents() {
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtEmail);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
