fbd0c1a5a7927b97c05ec6c7bdc629cc0d987034
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-10-16 12:05:55 +0200

    asdf

--- Acceso a Datos/Ej001/Ej1/src/dao/jugueteDAO.java
+++ Acceso a Datos/Ej001/Ej1/src/dao/jugueteDAO.java
@@ -8,11 +8,15 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public class jugueteDAO implements IDAO {
-    private String cadena = "";
-    public MotorDAO motorDAO;
+    public MotorDAO motorDAO = new MotorDAO();
 
-
-    @Override
+public void ejectuar(String asdf) {
+    motorDAO.connect();
+    switch (asdf) {
+        case "JUGUETES":
+            findall();
+    }
+}
     public ArrayList<juguete> findall() {
         try {
             ArrayList<juguete> lstjuguetes = new ArrayList<>();
@@ -21,19 +25,35 @@ public class jugueteDAO implements IDAO {
             ResultSet rs = motorDAO.executeQuery(SQL);
             while (rs.next()) {
                 juguete juguete = new juguete();
-                juguete.setId(rs.getInt("Id"));
-                juguete.setNombre(rs.getString("Nombre"));
-                juguete.setUnidad(rs.getInt("Unidad"));
-                juguete.setPrecio(rs.getInt("Precio"));
+                juguete.setId(rs.getInt(1));
+                juguete.setNombre(rs.getString(2));
+                juguete.setUnidad(rs.getInt(3));
+                juguete.setPrecio(rs.getInt(4));
                 lstjuguetes.add(juguete);
-
             }
 
             motorDAO.disconnect();
+
             return lstjuguetes;
 
         } catch (Exception e) {
             return null;
         }
     }
+
+    public static void main(String[] args) {
+        jugueteDAO dao = new jugueteDAO(); // Crear una instancia de la clase dao
+        ArrayList<juguete> lstjuguetes = dao.findall(); // Llamar al método findall para obtener la lista de juguetes
+
+        // Verificar si la lista no es nula antes de imprimir
+        if (lstjuguetes != null) {
+            // Imprimir el ArrayList en la consola
+            for (juguete j : lstjuguetes) {
+                System.out.println(j); // Esto imprimirá el objeto juguete utilizando su método toString()
+            }
+        } else {
+            System.out.println("Ocurrió un error al obtener la lista de juguetes.");
+        }
+    }
+
 }
