cc4fad37e89e09a3a3a58ae9fe222b42e82fb27f
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-10-18 10:13:35 +0200

    asdf

---
+++ Programacion/ANDROID/login/LoginAndroid_29_09_2023/app/src/main/java/com/example/loginandroid_29_09_2023/lst_films/model/entities/Movie.java
@@ -0,0 +1,7 @@
+package com.example.loginandroid_29_09_2023.lst_films.model.entities;
+
+public class Movie {
+    private static String ID = "ID";
+    private int id;
+    private String title, overview, poster_path;
+}
