f700c6ec83ebfcd60102efe040194c657caea55d
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-10-18 10:15:54 +0200

    xd

--- Programacion/ANDROID/login/LoginAndroid_29_09_2023/app/src/main/java/com/example/loginandroid_29_09_2023/lst_films/model/entities/Movie.java
+++ Programacion/ANDROID/login/LoginAndroid_29_09_2023/app/src/main/java/com/example/loginandroid_29_09_2023/lst_films/model/entities/Movie.java
@@ -1,10 +1,6 @@
 package com.example.loginandroid_29_09_2023.lst_films.model.entities;
 
 public class Movie {
-    private static String ID = "ID";
-
-    @RequestParam ("ID")
     private int id;
-    
     private String title, overview, poster_path;
 }
