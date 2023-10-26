9473b82b2ba4d11d9b502d4ca7f3ffdf47dd9cf7
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-10-26 19:59:37 +0200

    Revert "Revert "asdf""
    This reverts commit 4066a17de36221efe0ac8f8fe1bb73c51c713b8d.

--- Acceso a Datos/Ej001/Ej1/src/interfaces/IDAO.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-import java.util.ArrayList;
-
-public interface IDAO<E, I> {
-    public ArrayList<E> findall();
-}
--- Programacion/ANDROID/login/LoginAndroid_29_09_2023/app/src/main/java/com/example/loginandroid_29_09_2023/lst_films/model/entities/Movie.java
+++
@@ -1,6 +0,0 @@
-package com.example.loginandroid_29_09_2023.lst_films.model.entities;
-
-public class Movie {
-    private int id;
-    private String title, overview, poster_path;
-}
