1a68b39b6eaf65c0222a8f9ef1a53f1af2265c5d
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-11-02 16:57:10 +0100

    updatettette

--- Programacion/MoviesApp/app/src/main/java/com/example/moviesapp/Activities/MainActivity.java
+++ Programacion/MoviesApp/app/src/main/java/com/example/moviesapp/Activities/MainActivity.java
@@ -11,8 +11,53 @@ private Handler slideHandler = new Handler();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        banners();
+    }
+    private void banners() {
+        List<SliderItems> sliderItems = new ArrayList<>();
+        sliderItems.add(new SliderItems(R.drawable.wide1));
+        sliderItems.add(new SliderItems(R.drawable.wide2));
+        sliderItems.add(new SliderItems(R.drawable.wide3));
+
+        viewPager2.setAdapter(new SliderAdapters(sliderItems, viewPager2));
+        viewPager2.setClipToPadding(false);
+        viewPager2.setSitChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getOfChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = Math.abs(position);
+                page.setScaleY(0.85f+r*0.15f);
+            }
+        });
+        viewPager2.setPageTransformer(compositePageTransformer);
+        viewPager2.setCurrentItem(1);
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback(){
+
+        @Override
+        public void onPageSelected(int position) {
+            super.onPageSelected(position);
+            slideHandler.removeCallbacks(sliderRunnable);
+        }
+
+        });
+    }
+    private Runnable sliderRunnable = new Runnable() {
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem()+1);
+        }
+    }
+    @Override
+    protected void onPause() {
+        super.onResume();
+        slideHandler.postDelayed(sliderRunnable, 2000);
     }
     private void InitView() {
-        
+        viewPager2 = findViewById(R.id.viewpagerSlider)
     }
 }
--- Programacion/MoviesApp/app/src/main/java/com/example/moviesapp/Domain/SliderItems.java
+++ Programacion/MoviesApp/app/src/main/java/com/example/moviesapp/Domain/SliderItems.java
@@ -2,7 +2,9 @@ package com.example.moviesapp.Domain;
 
 public class SliderItems () {
     private int image;
-    
+    public SliderItems(int image) {
+        this.image = image;
+    }
     public int getImage() {
         return image;
     }
