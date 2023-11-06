8916b8b25f55871959f59052c28bd0961590f0bb
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2023-11-06 08:27:36 +0100

    aaaaaaaaa

---
+++ PSP/ThreadsCoding2/src/Hilo.java
@@ -0,0 +1,19 @@
+public class Hilo extends Thread{
+    private char id;
+
+    public Hilo(char id){
+        this.id = id;
+    }
+
+    public void run(Hilo hilo){
+        for (int i = 1; i < 10; i++) {
+            System.out.println("Soy el Hilo" + this.id + ".");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println("Soy el Hilo" + this.id + ", me acaban de despertar.");
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
